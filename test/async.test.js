5742ce0911b5aad4d863624af11633bd1852f897
Rizki Darmawan <rizdar.contact@gmail.com>
2023-07-22 10:50:28 +0700

    asyn mathers

--- test/async.test.js
+++ test/async.test.js
@@ -5,3 +5,8 @@ test('async function', async () => {
 
   expect(result).toBe('Hello Rizki');
 });
+
+test('test async matchers', async () => {
+  await expect(sayHelloAsync('Rizki')).resolves.toBe('Hello Rizki');
+  await expect(sayHelloAsync()).rejects.toBe('Name is empty');
+});
