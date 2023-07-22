5ddd422f128caa55b4c5efc30159436c17ba60f5
Rizki Darmawan <rizdar.contact@gmail.com>
2023-07-22 13:20:15 +0700

    set up code coverage

--- test/exeption.test.js
+++ test/exeption.test.js
@@ -5,3 +5,7 @@ test('exceptions', () => {
   expect(() => callMe('Rizki')).toThrow(MyExeption);
   expect(() => callMe('Rizki')).toThrow('Ups my exeptions happens');
 });
+
+test('exception not happens', () => {
+  expect(callMe('Budi')).toBe('OK');
+});
