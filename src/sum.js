4252281a103c023963631e08b031ee333769e696
Rizki Darmawan <rizdar.contact@gmail.com>
2023-07-23 19:47:23 +0700

    add sumAll function

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,12 @@
 export const sum = (first, second) => {
   return first + second;
 };
+
+export const sumAll = (value) => {
+  let total = 0;
+
+  for (const result of value) {
+    total += result;
+  }
+  return total;
+};
